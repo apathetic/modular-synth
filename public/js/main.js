dda8fcb5d84564da6c9fea5c12a57a52bcd54653
apathetic <wes.hatch@gmail.com>
2016-05-31 22:12:40 -0400

    i dont know

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,29 @@
 var µ = (function() {
 
-	var context = window.webkitAudioContext && (new window.webkitAudioContext());
-	var masterOut = context.destination;
-	var patch = angular.module("audio", []);
+	var context = window.AudioContext && (new window.AudioContext()),
+		patch,
+		masterOut;
 
 	if (!context) {
 		alert('Your crappy browser does not support webAudio');
 		return false;
 	}
+
+	patch = angular.module("audio", []);
+	masterOut = context.createGain();
+	masterOut.connect(context.destination);
+
+
+	// create a masterOut slider
+	// $(document.body).append(
+	// 	$compile(
+	// 		'<input type="range"....>'
+	// 	)(scope)
+	// );
+
+	// document.querySelector('#masterOut').addEventListener('change', function(){
+	// 	masterOut.gain.value = this.value;
+	// });
 
 	return {
 		context: context,
