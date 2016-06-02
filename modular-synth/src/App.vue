467ad7d1e808db23b3f156080625ccef190a574e
Wes Hatch <whatch@hugeinc.com>
2016-06-02 18:03:17 -0400

    cleaning up knob, oscillator

--- modular-synth/src/App.vue
+++ modular-synth/src/App.vue
@@ -4,10 +4,12 @@
 </template>
 
 <script>
+import Knob from './components/Knob';
 import Oscillator from './components/Oscillator';
 
 export default {
   components: {
+    Knob,
     Oscillator
   }
 };
--- modular-synth/src/main.js
+++ modular-synth/src/main.js
@@ -1,42 +1,25 @@
 import Vue from 'vue';
 import App from './App';
 
+var context = window.AudioContext && (new window.AudioContext());
+var masterOut = context.createGain();
+
+masterOut.connect(context.destination);
+
+Vue.mixin({
+  data: function() {
+    // All Components will have access to AudioContext
+    return {
+      context: context
+      // masterOut: masterOut
+    };
+  }
+});
+
+
+
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App }
 });
-
-
-var context = window.AudioContext && (new window.AudioContext());
-var masterOut = context.createGain();
-masterOut.connect(context.destination);
-
-// All Components have access to Audio patch stuffs
-Vue.mixin({
-  data: function() {
-    this.$data.context = context;
-    this.$data.masterOut = masterOut;
-  },
-  created: function() {
-    this.$data.context = context;
-    this.$data.masterOut = masterOut;
-    // var context = this.$options.context;
-    // var masterOut = this.$options.masterOut;
-
-    // this.context = context;
-    // this.masterOut = masterOut;
-    this.hello();
-  },
-  methods: {
-    hello: function() {
-      console.log('hello from mixin!');
-    }
-  }
-});
-
-
-// define a component that uses this mixin
-// var Component = Vue.extend({
-//   mixins: [patch]
-// })
