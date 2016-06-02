6fe79190f39ea479d9a2036e9f3f839e75a2901e
apathetic <wes.hatch@gmail.com>
2016-06-02 09:09:38 -0400

    updating component files

--- modular-synth/src/main.js
+++ modular-synth/src/main.js
@@ -1,8 +1,42 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App }
-})
+});
+
+
+var context = window.AudioContext && (new window.AudioContext());
+var masterOut = context.createGain();
+masterOut.connect(context.destination);
+
+// All Components have access to Audio patch stuffs
+Vue.mixin({
+  data: function() {
+    this.$data.context = context;
+    this.$data.masterOut = masterOut;
+  },
+  created: function() {
+    this.$data.context = context;
+    this.$data.masterOut = masterOut;
+    // var context = this.$options.context;
+    // var masterOut = this.$options.masterOut;
+
+    // this.context = context;
+    // this.masterOut = masterOut;
+    this.hello();
+  },
+  methods: {
+    hello: function() {
+      console.log('hello from mixin!');
+    }
+  }
+});
+
+
+// define a component that uses this mixin
+// var Component = Vue.extend({
+//   mixins: [patch]
+// })
