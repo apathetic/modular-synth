b1e442eaf73d61a527fddc261154235de2a45962
Wes Hatch <whatch@hugeinc.com>
2016-06-09 15:47:18 -0400

    updating components to be data-driven w/ vue

--- modular-synth/src/main.js
+++ modular-synth/src/main.js
@@ -3,19 +3,32 @@ import App from './App';
 
 var context = window.AudioContext && (new window.AudioContext());
 // var masterOut = context.createGain();
-
 // masterOut.connect(context.destination);
 
+// All Components will have access to AudioContext
 Vue.mixin({
   data: function() {
-    // All Components will have access to AudioContext
     return {
       context: context
-      // masterOut: masterOut
     };
   }
 });
 
+/* * /
+// Create Audio Input mixin
+// CREATE MIXIN from Node.vue?  AudioInput / ControlInputs / ...
+var inputs = {
+  methods: {
+    addInput: function() {
+      console.log('hello from mixin!');
+    }
+  }
+};
+console.log(inputs);
+// var Component = Vue.extend({
+//   mixins: [inputs, outputs,...]
+// })
+/* */
 
 
 /* eslint-disable no-new */
