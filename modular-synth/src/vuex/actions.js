3b41d84061ca8580aa96dd953cf2ae72230e4cfa
wes hatch <wes.hatch@gmail.com>
2016-08-06 10:42:01 -0400

    adding vuex, store

--- modular-synth/src/vuex/actions.js
+++ modular-synth/src/vuex/actions.js
@@ -13,7 +13,68 @@ export const incrementAsync = ({ dispatch }) => {
   }, 1000);
 };
 
+// //////////////////////////////////
 
 export const newModule = ({ dispatch }, type) => {
-  dispatch('NEW_MODULE', type);
+  dispatch('ADD_MODULE', type);
+};
+
+export const removeModule = ({ dispatch }, id) => {
+  dispatch('REMOVE_MODULE', id);
+};
+
+export const newConnection = ({ dispatch }, event, module, outlet) => {
+  // this.$dispatch('connector:new', {
+  //   module: this,
+  //   outlet: outlet,
+  //   el: event.target
+  // });
+
+  // keep this "end" of the line's source of truth in this
+  // component. Then, this node can update itself and the
+  // data shared in the Connector *should* update as well.
+  const from = {
+    module: module,
+    label: outlet.label,
+    data: outlet.data,
+    port: event.target,
+    connections: outlet.connections
+  };
+
+  console.log(from);
+  return;
+  /** /
+
+  const to = {}; // for now?
+  dispatch('ADD_CONNECTION', to, from);
+  /**/
+};
+
+
+
+export const updatePosition = ({ dispatch }, id, x, y) => {
+  dispatch('UPDATE_POSITION', id, x, y);
+
+  /* * /
+  if (module.inlets) {
+    module.inlets.forEach(function(inlet, i) {
+      inlet.connections.forEach(function(connector) {
+        connector.line.x2 = module.x - 3;
+        connector.line.y2 = module.y + (i * 20) + 17 + 80;  // if the inlets always maintain the same spacing...
+      });
+    });
+  }
+
+  if (module.outlets) {
+    module.outlets.forEach(function(outlet, i) {
+      outlet.connections.forEach(function(connector) {
+        dispatch('UPDATE_CONNECTION', connector, module);
+        // connector.line.x1 = module.x + module.width + 3;
+        // connector.line.y1 = module.y + (i * 20) + 17 + 80;
+      });
+    });
+  }
+  /* */
+
+  // dispatch('UPDATE_CONNECTIONS', module);
 };
--- modular-synth/src/vuex/store.js
+++ modular-synth/src/vuex/store.js
@@ -2,41 +2,62 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import plugins from './plugins';
 
-// Make vue aware of Vuex
 Vue.use(Vuex);
 
-var id = 0;
+let id = 0;
 
-export const STORAGE_KEY = 'wes';
+export const STORAGE_KEY_MODULES = 'modules';
+export const STORAGE_KEY_CONNECTIONS = 'connections';
+
 
 // Create an object to hold the initial state when the app starts up
 const state = {
-  // TODO: Set up our initial state
-  modules: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  modules: JSON.parse(localStorage.getItem(STORAGE_KEY_MODULES) || '[]'),
+  connections: JSON.parse(localStorage.getItem(STORAGE_KEY_CONNECTIONS) || '[]')
 };
 
 
 // Create an object storing various mutations. We will write the mutation
 const mutations = {
-  NEW_MODULE(state, type) {
-    // [TODO] use v-ref instead of idX
-    // var N = Vue.extend({
-    //   // props: {'type': type, idx: idx++},
-    //   data: () => ({'type': type, 'idx': idx++})
-    // });
-
+  ADD_MODULE(state, type) {
+    // FYI vue wraps push, splice, pop, etc. to trigger vue's reactivity updates
     state.modules.push({
+      id: id++,
       type: type,
-      id: id++
+      x: 0,
+      y: 0,
+      dragging: false
     });
+  },
+  REMOVE_MODULE(state, id) {
+    state.modules.splice(state.modules.indexOf(id), 1);
+
+    // return state.modules.filter((module) => {
+    //   module.id !== id;
+    // });
+  },
+  UPDATE_POSITION(state, id, x, y) {
+    state.modules[id].x = x;
+    state.modules[id].y = y;
+    // Vue.set(state.modules[id], 'x', x);
+    // Vue.set(state.modules[id], 'y', y);
+  },
+  ADD_CONNECTION(state, to, from) {
+    state.connections.push({
+      to: to,
+      from: from
+    });
+  },
+  REMOVE_CONNECTION(state, c) {
+    state.connections.splice(state.connections.indexOf(c), 1);
   }
 };
 
 
 // Combine the initial state and the mutations to create a Vuex store.
-// This store can be linked to our app.
 export default new Vuex.Store({
   state,
   mutations,
-  plugins
+  plugins,
+  strict: process.env.NODE_ENV !== 'production'
 });
