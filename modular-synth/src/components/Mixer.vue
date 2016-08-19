67fe3f78e06c96e94f0eaba1f80c579879e20272
Wes Hatch <whatch@hugeinc.com>
2016-08-19 12:35:27 -0400

    adding placeholder components

--- modular-synth/src/components/Mixer.vue
+++ modular-synth/src/components/Mixer.vue
@@ -3,38 +3,34 @@
     class="mixer module"
     :class="dragging ? 'dragging' : ''"
     :style="position"
-    @mousedown="startDraggingNode">
+    @mouseover.stop="setActiveModule(id)"
+    @mousedown.prevent="startDraggingNode">
+
+    <div class="module-details">
+      <h3>{{ name }}</h3>
+    </div>
 
     <div class="module-interface">
-      <h3>Mixer</h3>
+      <Level></Level>
       <Level></Level>
       <Level></Level>
     </div>
 
     <div class="module-connections">
-      <div class="inlets">
-        <span v-for="inlet in inlets"
-          data-label="{{ inlet.label }}"
-          class="inlet">
-        </span>
-      </div>
-      <div class="outlets">
-        <span v-for="outlet in outlets"
-          @mousedown.stop="createConnector($event, outlet)"
-          data-label="{{ outlet.label }}"
-          class="outlet">
-        </span>
-      </div>
+      <partial name="inlets"></partial>
+      <partial name="outlets"></partial>
     </div>
+  </div>
 </template>
 
 <script>
-import {draggable} from '../mixins';
+import { draggable } from '../mixins';
 import Level from './UI/Level';
 
 export default {
+  props: { id: null },
   mixins: [draggable],
-  components: {Level},
+  components: { Level },
 
   data() {
     return {
@@ -42,77 +38,64 @@ export default {
 
       inlets: [
         {
-          label: 'in 1L',
-          data: this.in1L,  // to
-          connections: []
+          label: 'in-1',
+          data: null
         }, {
-          label: 'in 1R',
-          data: this.in1R,  // to
-          connections: []
-        },
-        {
-          label: 'in 2L',
-          data: this.in2L,  // to
-          connections: []
+          label: 'in-2',
+          data: null
         }, {
-          label: 'in 2R',
-          data: this.in2R,  // to
-          connections: []
-        },
-        {
-          label: 'in 3L',
-          data: this.in3L,  // to
-          connections: []
+          label: 'in-3',
+          data: null
         }, {
-          label: 'in 3R',
-          data: this.in3R,  // to
-          connections: []
+          label: 'in 4',
+          data: null
+        }, {
+          label: 'in-5',
+          data: null
+        }, {
+          label: 'in-6',
+          data: null
         }
       ],
 
       outlets: [
         {
-          label: 'out L',
-          data: this.outL,  // to
-          connections: []
+          label: 'out-1',
+          data: null
         }, {
-          label: 'out R',
-          data: this.outR,  // to
-          connections: []
+          label: 'out-2',
+          data: null
         }
       ]
     };
   },
   ready() {
-    this.in1L = this.context.createGain();
-    this.in1R = this.context.createGain();
-    this.in2L = this.context.createGain();
-    this.in2R = this.context.createGain();
-    this.in3L = this.context.createGain();
-    this.in3R = this.context.createGain();
+    // inputs
+    this.inlets[0].data = this.context.createGain();
+    this.inlets[1].data = this.context.createGain();
+    this.inlets[2].data = this.context.createGain();
+    this.inlets[3].data = this.context.createGain();
+    this.inlets[4].data = this.context.createGain();
+    this.inlets[5].data = this.context.createGain();
 
-    this.outL = this.context.createGain();
-    this.outR = this.context.createGain();
+    // outputs
+    this.outlets[0].data = this.context.createGain();
+    this.outlets[1].data = this.context.createGain();
 
-    this.in1L.connect(this.outL);
-    this.in1R.connect(this.outR);
-    this.in2L.connect(this.outL);
-    this.in2R.connect(this.outR);
-    this.in3L.connect(this.outL);
-    this.in3R.connect(this.outR);
-
-    var e = this.$el;
-
-    e.id = 'module-' + this.idx;
-    e.style.left = '200px';
-    e.style.top = '200px';
+    // connectify
+    this.inlets[0].data.connect(this.outlets[0].data);
+    this.inlets[1].data.connect(this.outlets[1].data);
+    this.inlets[2].data.connect(this.outlets[0].data);
+    this.inlets[3].data.connect(this.outlets[1].data);
+    this.inlets[4].data.connect(this.outlets[0].data);
+    this.inlets[5].data.connect(this.outlets[1].data);
   }
 };
 </script>
 
 <style type="sass">
-  .mixer {
+  /*.mixer {
     width: 380px;
     height: 250px;
-  }
+  }*/
 </style>
