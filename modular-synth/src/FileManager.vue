9b65e0e20757853a7f4aedadc7783118945879a5
apathetic <wes.hatch@gmail.com>
2016-08-22 23:06:03 -0400

    filemehnager

--- modular-synth/src/FileManager.vue
+++ modular-synth/src/FileManager.vue
@@ -2,12 +2,15 @@
 //  LOAD / SAVE
 // -----------------------------------------------
 
+// TODO this is just to get json into the App, dynamically. Will need to make more
+// load-friendly ie. proper ajax implementation. Or just bundle json with the App.
+
 <template>
   <button>save</button>
   <select name="load">
     <option value="juno">Juno</option>
     <option value="arp">Arp</option>
-    <option v-for="patch in data">{{ patch.name }}</option>
+    <option v-for="patch in patches" :value="patch.name">{{ patch.name }}</option>
   </select>
   <button>load</button>
 </template>
@@ -16,7 +19,7 @@
 
 import * as actions from './vuex/actions';
 // import * as patches from './assets/patches';
-import FM from './assets/patches/FM.json';
+import FM from './assets/patches/FM';
 // import Juno from './assets/patches/Juno';
 
 export default {
@@ -26,10 +29,10 @@ export default {
     },
     actions: actions
   },
-  data() {
-    return {
+  data: {
+    patches: [
       FM
-    };
+    ]
   },
   ready() {
     console.log('asfdfasasdfasdfafsd');
