3114f8b0989b9dc6f180cb1ecee7b85ceb958530
wes hatch <wes.hatch@gmail.com>
2016-09-19 00:30:08 -0400

    updating grid logic when dragging module

--- modular-synth/src/vuex/store.js
+++ modular-synth/src/vuex/store.js
@@ -99,8 +99,6 @@ const mutations = {
       y: 0,         // for draggingY
       col: 0,       // for gridX position
       row: 0,       // for gridY position
-      // width -- should be unique the the node
-      // height -- should be unique the the node
       w: 1,         // but, set for now just to get it working
       h: 1
     });
