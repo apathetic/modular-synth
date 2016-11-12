cdb9f8f4dd60862b58e68dec9799b1486f8ac84e
apathetic <wes.hatch@gmail.com>
2016-11-12 18:15:59 -0500

    much cleaner connector dragend logic

--- modular-synth/src/main.js
+++ modular-synth/src/main.js
@@ -17,8 +17,8 @@ Vue.mixin({
   }
 });
 
-Vue.partial('inlets', '<div class="inlets"><span v-for="inlet in inlets" data-label="{{ inlet.label }}" class="inlet"></span></div>');
-Vue.partial('outlets', '<div class="outlets"><span v-for="outlet in outlets" @mousedown.stop="newConnection(outlet)" data-label="{{ outlet.label }}" class="outlet"></span></div>');
+Vue.partial('inlets', '<div class="inlets"><span v-for="inlet in inlets" data-label="{{ inlet.label }}" data-port="{{ $index }}" class="inlet"></span></div>');
+Vue.partial('outlets', '<div class="outlets"><span v-for="outlet in outlets" @mousedown.stop="newConnection(outlet)" data-label="{{ outlet.label }}" data-port="{{ $index }}" class="outlet"></span></div>');
 
 
 /* eslint-disable no-new */
