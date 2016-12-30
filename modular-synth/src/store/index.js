517613bef2addff858bfd47c80d79546e089f5f6
apathetic <wes.hatch@gmail.com>
2016-12-30 10:49:31 -0800

    updating store into partials

--- modular-synth/src/store/index.js
+++ modular-synth/src/store/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import getters from './getters';
 import mutations from './mutations';
-import actions from './actions';
+// import actions from './actions';
+// import getters from './getters';
+
+import * as actions from './actions';
+import * as getters from './getters';
+
 import plugins from './plugins';
 
 Vue.use(Vuex);
