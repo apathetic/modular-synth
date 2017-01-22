c50424a1e5db6ef3825a6b165f7b8d5360d6fd15
apathetic <wes.hatch@gmail.com>
2017-01-22 14:28:25 -0500

    adding some boilerplate tests

---
+++ modular-synth/test/e2e/connector.spec.js
@@ -0,0 +1,29 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+/**
+ * Test that user can login and see dashboard.
+ */
+module.exports = {
+  'connector e2e tests': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#modules', 5000)
+
+      // Assert that user can create a connection.
+
+      // Assert that user can remove a connection.
+
+      // Assert that audio is routed correctly after a new connection
+
+      // Assert that audio is disconnected after removing a connection
+
+      .pause(2000)
+      .end()
+  }
+}
---
+++ modular-synth/test/e2e/sortable.spec.js
@@ -0,0 +1,30 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+/**
+ * Test that user can login and see dashboard.
+ */
+module.exports = {
+  'sortable e2e tests': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#modules', 1000)
+
+      // Assert that App can sort modules in play mode
+
+      // Assert that sortable works after load
+
+      // Assert that sortable works after adding a module
+
+      // Assert that sortable works after removing a module
+
+
+      .pause(2000)
+      .end()
+  }
+}
