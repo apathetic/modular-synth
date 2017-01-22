7ebb13c7abe734e13f8abf991062e40e6ba1307c
apathetic <wes.hatch@gmail.com>
2017-01-21 20:21:01 -0500

    filemanager now fetches json patches from endpoint

--- modular-synth/src/store/index.js
+++ modular-synth/src/store/index.js
@@ -8,8 +8,9 @@ import * as getters from './getters';
 Vue.use(Vuex);
 
 
-export const STORAGE_KEY_MODULES = 'modules';
-export const STORAGE_KEY_CONNECTIONS = 'connections';
+export const LS_NAME = 'name';
+export const LS_MODULES = 'modules';
+export const LS_CONNECTIONS = 'connections';
 
 
 // -----------------------------------------------
@@ -17,12 +18,13 @@ export const STORAGE_KEY_CONNECTIONS = 'connections';
 // -----------------------------------------------
 
 const state = {
+  name: localStorage.getItem(LS_NAME) || '',
   id: parseInt(localStorage.getItem('id')) || 0,    // NOTE: this will actually start at 1 (we increment just prior to adding new), as masterOut is 0
-  modules: JSON.parse(localStorage.getItem(STORAGE_KEY_MODULES) || '[{"type": "MasterOut", "id": 0, "x": 0, "y": 0}]'),
-  // connections: JSON.parse(localStorage.getItem(STORAGE_KEY_CONNECTIONS) || '[]'),
+  modules: JSON.parse(localStorage.getItem(LS_MODULES) || '[{"type": "MasterOut", "id": 0, "x": 0, "y": 0}]'),
   connections: undefined,   // NOTE: this is intentional to force a delayed "loading" of the connectors. We grab them
                             // in localStorage manually in the webAudioPlugin _after_ all the modules have loaded in
                             // order to ensure audio-routing and visual connections will have Objects to link to / from
+                            // nope => connections: JSON.parse(localStorage.getItem(LS_CONNECTIONS) || '[]'),
   editing: false,
   focused: undefined,       // "Hovered": for Module Info, Connections.  TODO move to $bus?
   active: 0                 // "Clicked": for Dragging, Deleting.
--- modular-synth/src/store/plugins.js
+++ modular-synth/src/store/plugins.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
-import { STORAGE_KEY_MODULES, STORAGE_KEY_CONNECTIONS } from './index';
+import { LS_NAME, LS_MODULES, LS_CONNECTIONS } from './index';
 // import createLogger from 'vuex/logger';
 
 
 const localStoragePlugin = (store) => {
+  store.subscribe((mutation, { name }) => {
+    localStorage.setItem(LS_NAME, name);
+  });
   store.subscribe((mutation, { modules }) => {
-    localStorage.setItem(STORAGE_KEY_MODULES, JSON.stringify(modules));
+    localStorage.setItem(LS_MODULES, JSON.stringify(modules));
   });
   store.subscribe((mutation, { connections }) => {
     if (connections !== undefined) {
-      localStorage.setItem(STORAGE_KEY_CONNECTIONS, JSON.stringify(connections));
+      localStorage.setItem(LS_CONNECTIONS, JSON.stringify(connections));
     }
   });
   store.subscribe((mutation, { id }) => {
@@ -22,7 +25,7 @@ const webAudioPlugin = (store) => {
     if (mutation.type === 'LOAD') {
       Vue.nextTick(function() {   // WHYWHYWHY TODO TODO ????
         console.log('Nodes loaded, now routing audio...');
-        store.state.connections = JSON.parse(localStorage.getItem(STORAGE_KEY_CONNECTIONS) || '[]');
+        store.state.connections = JSON.parse(localStorage.getItem(LS_CONNECTIONS) || '[]');
       });
       // end of WTF ------------------------------
     }
