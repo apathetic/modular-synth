ca043fbf98e82e957646d706ab258f2328655d12
Wes Hatch <whatch@hugeinc.com>
2017-02-20 10:39:10 -0500

    updating filter interface

--- modular-synth/build/dev-server.js
+++ modular-synth/build/dev-server.js
@@ -71,7 +71,7 @@ module.exports = app.listen(port, function (err) {
   }
 
   // when env is testing, don't need open it
-  if (process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
+  // if (process.env.NODE_ENV !== 'testing') {
+  //   opn(uri)
+  // }
 })
