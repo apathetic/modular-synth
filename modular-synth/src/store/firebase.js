882d98571c636a741a58b3db69e290a7e46baeb7
apathetic <wes.hatch@gmail.com>
2017-03-15 00:44:14 -0400

    working authentication

--- modular-synth/src/store/firebase.js
+++ modular-synth/src/store/firebase.js
@@ -5,83 +5,36 @@ import 'firebase/database'; // import database into firebase namespace
 
 firebase.initializeApp(config);
 
-const _auth = firebase.auth();
-const _database = firebase.database();
+const database = firebase.database();
 
-
-// ----
-
-_auth.onAuthStateChanged(function(user) {
-  if (user) {
-    // saveToken();
-    var photoURL = user.photoURL;
-    var name = user.displayName;
-    var refreshToken = user.refreshToken;
-
-    console.log(photoURL, name, refreshToken);
-  } else {
-
-  }
-});
-
-function isSignedIn() {
-  if (_auth.currentUser) {
-    return true;
-  }
-
-  // Display a message to the user and return false
-  // ...
-  return true;
-};
-
-// ----
-
-
-
-export const auth = {
-  signIn() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    _auth.signInWithPopup(provider);
-  },
-
-  signOut() {
-    _auth.signOut();
-  },
-
-  isSignedIn: isSignedIn
-};
-
+export const auth = firebase.auth;
 export const api = {
   // Load data ONCE (ie. no "listeners")
   // path: ie: /users/' + userId
   load(path) {
-    if (isSignedIn()) {
-      return _database.ref(path).once('value');
-    }
+    return database.ref(path).once('value');
   },
 
   // list(path) {
-  //   return _database.ref(path + '?shallow=true');  // NOTE just to illustrate what trying to do. this won't work
+  //   return database.ref(path + '?shallow=true');  // NOTE just to illustrate what trying to do. this won't work
   // },
 
   // note: destructive. Will overwrite if anything happens to be at this path
   create(name, data) {
-    _database.ref('patch/' + name).set({});
+    return database.ref('patch/' + name).set({});
   },
 
   save(path, data) {
-    if (isSignedIn()) {
-      return _database.ref(path).update(data);
-    }
+    return database.ref(path).update(data);
   },
 
   add(path, data) {
-    const currentUser = _auth.currentUser;
-    const items = _database.ref(path);   // ie. patch, or patch/juno/parameterSets
+    // const currentUser = _auth.currentUser;
+    const items = database.ref(path);   // ie. patch, or patch/juno/parameterSets
 
     // Add a new message entry to the Firebase Database.
     items.push({    // data
-      name: currentUser.displayName,
+      // name: currentUser.displayName,
       text: this.messageInput.value
     }).then(function() {
       // do whatever
