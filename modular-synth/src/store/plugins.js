2177486e9caf23096aeba04dba0956893a38edc2
apathetic <wes.hatch@gmail.com>
2017-06-13 00:09:49 -0400

    moving towards arrays for paramsets and patches

--- modular-synth/src/store/plugins.js
+++ modular-synth/src/store/plugins.js
@@ -1,10 +1,12 @@
-import { PATCH_KEY, MODULES_KEY, CONNECTIONS_KEY, PARAMETERS_KEY } from './index';
-// import createLogger from 'vuex/logger';
-
+import { KEY, NAME_KEY, MODULES_KEY, CONNECTIONS_KEY, PARAMETERS_KEY } from './index';
 
 const localStoragePlugin = (store) => {
   store.subscribe((mutation, { key }) => {
-    localStorage.setItem(PATCH_KEY, key);
+    // if (mutation.type === 'LOAD_PATCH') {
+    localStorage.setItem(KEY, key);
+  });
+  store.subscribe((mutation, { name }) => {
+    localStorage.setItem(NAME_KEY, name);
   });
   store.subscribe((mutation, { modules }) => {
     localStorage.setItem(MODULES_KEY, JSON.stringify(modules));
@@ -20,24 +22,4 @@ const localStoragePlugin = (store) => {
   });
 };
 
-const webAudioPlugin = (store) => {
-  store.subscribe((mutation) => {
-    if (mutation.type === 'LOAD_PATCH') {
-      // store.state.modules.forEach((module) => {
-      //   if ((!+module.col || !+module.row) && !!module.id) {   // quick check that row / col are not null, undef.
-      //     console.log('ERROR: %s #%d does not have rack coords', module.type, module.id);
-      //     module.row = 0;
-      //     module.col = 0;
-      //   }
-      // });
-
-      // Vue.nextTick(function() {   // ensure nodes (+ inlets/outlets) are in the DOM
-      //   console.log('All modules loaded, now routing audio...');
-      //   store.state.connections = JSON.parse(localStorage.getItem(CONNECTIONS_KEY) || '[]');
-      // });
-    }
-  });
-};
-
-
-export default [localStoragePlugin, webAudioPlugin];
+export default [localStoragePlugin];
