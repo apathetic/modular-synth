5529b6aea124b7570fb7534638ff69296db3507a
Wes Hatch <whatch@hugeinc.com>
2017-08-13 21:30:52 -0300

    fallbacks if not logged in;

--- modular-synth/build/dev-server.js
+++ modular-synth/build/dev-server.js
@@ -4,7 +4,7 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = JSON.parse(config.dev.env.NODE
 var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
-var opn = require('opn')
+// var opn = require('opn')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = process.env.NODE_ENV === 'testing'
   ? require('./webpack.prod.conf')
