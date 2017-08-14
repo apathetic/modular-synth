1ec2468e1fa4ae64ef24d49bdfede920afb7b619
apathetic <wes.hatch@gmail.com>
2017-08-14 18:24:05 -0300

    patchmanager: everything now in computed props

--- modular-synth/src/store/actions.js
+++ modular-synth/src/store/actions.js
@@ -16,13 +16,14 @@ let blank = {   // TODO make this (object) live somewhere universal. schema js ?
 // -----------------------------------------------
 
 /**
- * Load a Patch from the Store; only patches that have been previously
- * fetched may be loaded.
+ * Load a Patch from the Store; patches that have been previously fetched and
+ * cached may be loaded directly, else fallback to localStorage.
  *
  * NOTE: we cannot load modules, connections, and parameters all at once.
  *       Modules must be mounted _first_, so that the AudioNode is available
  *       to the Connections; likewise with the Modules' settings being
  *       available to the Parameters.
+ *
  * @param  {[type]} commit [description]
  * @param  {[type]} state  [description]
  * @param  {String} key    The Object key of the patch to load
@@ -35,28 +36,28 @@ export const loadPatch = ({ commit, state }, key) => {
 
   // if loading patch via a specific key
   if (key && state.patches[key]) {
-    console.log('%c Loading patch: %s', 'background:#666;color:white;', key);
+    console.log('%c Loading patch: %s ', 'background:#666;color:white;font-weight:bold;', key);
     patch = state.patches[key];
     commit('SET_KEY', key);
   } else {
-    console.log('%c Loading patch from localStorage ', 'background:#666;color:white;');
+    console.log('%c Loading patch from localStorage ', 'background:#666;color:white;font-weight:bold;');
     // TODO store this object somewhere global. USE blank, ABOVE
     patch = {
       id: parseInt(localStorage.getItem('id')) || 0,
-      name: parseInt(localStorage.getItem(_NAME)) || '<blank>',
+      name: localStorage.getItem(_NAME) || '',
       modules: JSON.parse(localStorage.getItem(_MODULES)) || [{'type': 'MasterOut', 'id': 0, 'x': 0, 'y': 0}],
       connections: JSON.parse(localStorage.getItem(_CONNECTIONS) || '[]'),
       parameterSets: JSON.parse(localStorage.getItem(_PARAMETERS) || '[]')
     };
   }
 
-  // loads: id, name and modules ... (and parameterSets??) ...
+  // load: id, name, modules, and parameterSets
   commit('LOAD_PATCH', patch);
 
   // ensure nodes (+ inlets/outlets) are in the DOM...
+  // ...then load connections (from the same patch Object) ...
   Vue.nextTick(function() {
     console.log('All modules loaded, now routing audio...');
-    // ...then load connections (from the same patch Object) ...
     commit('LOAD_CONNECTIONS', patch);
 
     // ...lastly, load parameters (from the same patch Object)
