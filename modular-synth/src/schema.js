4939ed8565c14f2f15e435b7526f806aeba3e2a4
apathetic <wes.hatch@gmail.com>
2017-10-02 20:20:25 -0400

    code cleanup

--- modular-synth/src/schema.js
+++ modular-synth/src/schema.js
@@ -1,59 +1,32 @@
+
 /*
-const Schema = {
-  type: Object,
-  properties:  {
-    "name" : {
-      type: String,
-    },
-    "id" : {
-      type: Integer
-    },
-    "connections" : {
-      type: Array,
-      items: {
-        type: Object,
-        properties: {
-          "id" : 21,
-          "from" : {
-            "id" : 20,
-            "port" : 0
-          },
-          "to" : {
-            "id" : 0,
-            "port" : 0
-          }
-        }
-      }
-    },
-    "modules" : {
-      type: Array,
-      items: {
-        "id" : Integer
-        "type" : String,
-        "col" : 0,
-        "row" : 1,
-        "w" : 4,
-        "h" : 1,
-        "x" : 291,
-        "y" : 220
-      }
-    }
-    "parameterSets" : {
-      type: Array,
-      items: {
-        type: Object,
-        properties: {
-          "name" : {
-            type: String
-          },
-          "parameters" : {
-            type: Mixed
-          }
-        }
-      }
-    }
+
+class DataApi {
+  constructor(rawData) {
+    this.rawData = rawData;
   }
+  mapIntoObject(arr) {
+    return arr.reduce((acc, curr) => {
+      acc[curr.id] = curr;
+      return acc;
+    }, {});
+  }
+  getArticles() {
+    return this.mapIntoObject(this.rawData.articles);
+  }
+  getAuthors() {
+    return this.mapIntoObject(this.rawData.authors);
+  }
+}
+
+export default DataApi;
 */
+
+
+
+
+
+
 
 /**
  * This validates the Patch Object coming from Firebase, and
@@ -110,19 +83,3 @@ export function validateData(patches) {
 
   return patches;
 }
-
-// /**
-//  * Asserts the field is present and correct. If not, tries to fix.
-//  * @param  {[type]} assert [description]
-//  * @param  {[type]} name   [description]
-//  * @param  {[type]} type   [description]
-//  * @return {[type]}        [description]
-//  */
-// function check(assert, name, type) {
-//   if (assert) {
-//     return;
-//   } else {
-//     console.warn('Problem with missing "%s" in patch', name);
-//     assert
-//   }
-// }
