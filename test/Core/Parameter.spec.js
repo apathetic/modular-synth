5208a4cb8b0236cb5abdf4d6be9861970d0f267e
apathetic <wes.hatch@gmail.com>
2017-10-24 20:18:45 -0400

    fixing test branch

---
+++ test/Core/Parameter.spec.js
@@ -0,0 +1,11 @@
+import { shallow, mount, createLocalVue } from 'vue-test-utils'
+import { createRenderer } from 'vue-server-renderer'
+import { Parameter } from '@/audio'
+
+const localVue = createLocalVue();
+
+describe('Parameter', () => {
+  it('handles input connections', function(){});
+
+  it('can set a value', function(){});
+});
