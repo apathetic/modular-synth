56aba891ba31747c157d59769b2d1fb9ff2d6908
apathetic <wes.hatch@gmail.com>
2017-11-07 21:27:52 -0500

    migrating tests

--- cypress/integration/core/context.spec.js
+++ cypress/integration/core/context.spec.js
@@ -1,29 +1,18 @@
-import Puppeteer from 'Puppeteer';
-import { context } from '../src/audio/index';
+import { context } from '../../../src/audio/index';
 
 describe('context', () => {
-  let browser, page;
+  it ('exists', () => {
+    expect(context).to.be.instanceof(AudioContext); // sanity check that our test env works
+  });
 
-  beforeEach(async () => {
-    browser = await puppeteer.launch({ headless: true });
-    page = await browser.newPage();
-  }, 1000 * 30);
-
-  afterEach(() => {
-    browser.close()
-  }, 1000 * 30);
-
-
-
-  it ('exists...?', () => {
+  it ('has the required Audio Nodes', () => {
     expect(context).to.have.property('createGain');
     expect(context.createGain()).to.be.instanceOf(GainNode);
     expect(context).to.have.property('createOscillator');
     expect(context.createOscillator()).to.be.instanceOf(OscillatorNode);
     expect(context).to.have.property('createDelay');
     expect(context.createDelay()).to.be.instanceOf(DelayNode);
-
-    return context.close();
   });
 
+  // context.close();
 });
