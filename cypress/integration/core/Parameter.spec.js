d1b06344460bd3ea5ea6148b1e0fc021d825dd6e
wes hatch <wes.hatch@gmail.com>
2017-11-07 22:01:12 -0500

    updating tests

--- cypress/integration/core/Parameter.spec.js
+++ cypress/integration/core/Parameter.spec.js
@@ -1,11 +1,34 @@
-import { shallow, mount, createLocalVue } from 'vue-test-utils'
-import { createRenderer } from 'vue-server-renderer'
-import { Parameter } from '@/audio'
+// import { shallow, mount, createLocalVue } from 'vue-test-utils'
+// import { createRenderer } from 'vue-server-renderer'
+import { Parameter } from '../../../src/audio';
+import dummy from '../../../src/components/Node.vue';
+// import { dummy } from '../../support/dummy';
+// import { VCO } from '/src/components/node';
 
-const localVue = createLocalVue();
+// const localVue = createLocalVue();
 
 describe('Parameter', () => {
-  it('handles input connections', function(){});
+  it('handles input connections', () => {
+    const param = new Parameter();
+    dummy.output.connect(param.input);
+  });
 
-  it('can set a value', function(){});
+  it('handles output connections', () => {
+    const param = new Parameter();
+    param.input.connect(dummy.output);
+  });
+
+  it('can set a value', () => {
+    const value = 10;
+    const param = new Parameter(value);
+
+    // expect
+  });
+
+  it('can be cleaned up', () => {
+    const param = new Parameter();
+    param.destroy();
+
+    // expect
+  });
 });
