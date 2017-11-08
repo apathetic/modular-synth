5845fad326e0c914ad7758093e7a232bd2da429f
wes hatch <wes.hatch@gmail.com>
2017-11-07 22:01:12 -0500

    updating tests

--- cypress/integration/core/sortable.spec.js
+++ cypress/integration/core/sortable.spec.js
@@ -4,6 +4,22 @@ import Node from '@/components/Node';
 
 describe('sortable', () => {
 
+  // --------
+
+  function movePiece (number, x, y) {
+    cy.get(`.piece-${number}`)
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { clientX: x, clientY: y })
+      .trigger('mouseup', {force: true})
+  };
+
+  beforeEach(function(){
+    cy.viewport(550, 350)
+    cy.visit('http://localhost:8080/')
+  });
+
+  // --------
+
   it('the App can sort modules in play mode', () => {
     const App = shallow(Synth, {
       stubs: {
@@ -11,6 +27,9 @@ describe('sortable', () => {
         Node
       }
     });
+
+    movePiece(1, 340, 130)
+    cy.get('.pieces li').eq(3).find('span').should('not.exist')
 
     // ...
   });
