1033e6228c9bcffa5a769cbcc402cd24ebb45224
wes hatch <wes.hatch@gmail.com>
2017-11-09 01:09:59 -0500

    updating webpack and assoc. modules

--- config/index.js
+++ config/index.js
@@ -1,13 +1,14 @@
-// see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
 module.exports = {
   build: {
-    env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
+
+    // paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -17,8 +18,10 @@ module.exports = {
     productionGzipExtensions: ['js', 'css']
   },
   dev: {
-    env: require('./dev.env'),
-    port: 8080,
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
+
+    // paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
@@ -26,6 +29,13 @@ module.exports = {
       //   target: 'http://localhost:8081'
       // }
     },
+
+    // dev server settings
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false,
+
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
