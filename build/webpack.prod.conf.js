43625e91cb829ff20b06aaa4b5dec10a7d78be20
apathetic <wes.hatch@gmail.com>
2017-11-11 12:07:57 -0500

    really     adding landing auth page

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,6 +24,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    // publicPath: config.build.assetsPublicPath
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
