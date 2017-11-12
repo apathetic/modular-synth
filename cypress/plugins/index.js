115f265b6ce2e082596eb3ae49af77206c4fa089
apathetic <wes.hatch@gmail.com>
2017-11-11 20:42:14 -0500

    tweaks

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,7 +2,7 @@ const wp = require('@cypress/webpack-preprocessor')
 
 module.exports = (on) => {
   const options = {
-    webpackOptions: require('../../build/webpack.base.conf');
+    webpackOptions: require('../../build/webpack.test.conf');
   }
   on('file:preprocessor', wp(options))
 }
