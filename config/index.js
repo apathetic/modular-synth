456d94e29ee3a5b8e8d34ae46b634fc10e908368
apathetic <wes.hatch@gmail.com>
2017-11-24 16:03:11 -0500

    updating config

--- config/index.js
+++ config/index.js
@@ -2,42 +2,30 @@ var path = require('path')
 
 module.exports = {
   build: {
-    env: require('./prod.env'),
+    env: { NODE_ENV: JSON.stringify('production')},
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    productionSourceMap: true,
+    cssSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: false
   },
   dev: {
-    env: require('./dev.env'),
+    env: { NODE_ENV: JSON.stringify('development')},
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT
     assetsSubDirectory: 'static',
     proxyTable: {
       // '/api': {
       //   target: 'http://localhost:8081'
       // }
     },
-
-    // dev server settings
     poll: false,
-
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
     cssSourceMap: false
   }
 }
