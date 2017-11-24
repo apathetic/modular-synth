49b326f5b8b7460c5608923099b3758002246e27
apathetic <wes.hatch@gmail.com>
2017-11-24 02:46:22 -0500

    fixing cypress webpack plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,7 @@
-const webpack = require('@cypress/webpack-preprocessor')
-const options = {
-  webpackOptions: require('../../build/webpack.base.conf');
-};
+const webpack = require('@cypress/webpack-preprocessor');
+const config = require('../../build/webpack.dev.conf');
+const options = { webpackOptions: config };
 
 module.exports = (on) => {
-  on('file:preprocessor', webpack(options))
+  on('file:preprocessor', webpack(options));
 }
