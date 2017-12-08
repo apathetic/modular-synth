c73c16f9e13dc79ffeb650c8aa5bc5e90a28dcf7
apathetic <wes.hatch@gmail.com>
2017-12-07 22:27:24 -0500

    adding root module idea

--- src/components/system/Module.vue
+++ src/components/system/Module.vue
@@ -1,75 +1,49 @@
 <template>
   <div
-      class="module"
-      :class="[width, tall, isDragging ? 'dragging' : '']"
-      :style="position"
-      @mousedown.stop="startDragging">
+    class="oscillator module _4U"
+    :class="dragging ? 'dragging' : ''"
+    :style="position"
+    @mousedown.stop="startDragging">
 
-    <component
-      :is="module.type"
-      :id="module.id">
-    </component>
+    <div class="module-details">
+      <h3>{{ name }}</h3>
+    </div>
 
+    <div class="module-interface">
+      <slot></slot>
+    </div>
+
+    <div class="module-connections">
+      <inlets  :ports="inlets"></inlets>
+      <outlets :ports="outlets"></outlets>
+    </div>
   </div>
 </template>
 
 <script>
   import { draggable } from '@/mixins/draggable';
-  import { moduleSize } from '../../dimensions';
-
-  import Analyser from '../Analyser';
-  import Comb from '../Comb';
-  import Compressor from '../Compressor';
-  import Delay from '../Delay';
-  import Drive from '../Drive';
-  import Env from '../Env';
-  import LFO from '../LFO';
-  import Mixer from '../Mixer';
-  import NoteIn from '../NoteIn';
-  import OSC from '../OSC';
-  import Reverb from '../Reverb';
-  import VCF from '../Filter';
-  import VCO from '../VCO';
-  import VCA from '../VCA';
-
-  import Debugger from '../Debugger';
-  import Node from '../Node';
 
   export default {
     mixins: [draggable],
-    components: {
-      Analyser,
-      Comb,
-      Compressor,
-      Debugger,
-      Delay,
-      Drive,
-      Env,
-      LFO,
-      Mixer,
-      Node,
-      NoteIn,
-      OSC,
-      Reverb,
-      VCF,
-      VCO,
-      VCA
-    },
-
     props: {
-      module: Object
+      col: 0,
+      row: 0,
+      name: 'module',
+      inlets: [],
+      outlets: [],
+      width: null
     },
 
     created() {
-      this.id = this.module.id;
-      this.width = `_${moduleSize[this.module.type][0]}U`;
-      this.tall = moduleSize[this.module.type][2] ? 'module--tall' : '';
-
-      console.log('%c[component] Creating %s', 'color: blue', this.module.type);
+      console.log('%c[component] Creating VCO', 'color: blue');
     },
 
     destroyed() {
-      console.log('Destroying %s ', this.module.type);
+      console.log('Destroying VCO ', this.id);
     }
   };
 </script>
+
+<style lang="scss">
+  @import '../../assets/scss/variables.scss';
+</style>
