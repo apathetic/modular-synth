dad4804ecee1e7a5fe0a84f3eab1c3c8d5c43891
apathetic <wes.hatch@gmail.com>
2018-01-01 10:05:58 -0800

    no longer necessart to pass module obj down

--- src/components/system/Module.vue
+++ src/components/system/Module.vue
@@ -1,47 +1,74 @@
 <template>
   <div
-    class="module _4U">
-    :class="[name, '_' + width, {dragging: dragging}]"
-    :style="position"
-    @mousedown.stop="startDragging"
+      class="module"
+      :class="[width, isDragging ? 'dragging' : '']"
+      :style="position"
+      @mousedown.stop="startDragging">
 
-    <div class="module-details">
-      <h3>{{ name }}</h3>
-    </div>
+    <component
+      :is="module.type"
+      :id="module.id">
+    </component>
 
-    <div class="module-interface">
-      <slot></slot>
-    </div>
-
-    <div class="module-connections">
-      <inlets  :ports="inlets"></inlets>
-      <outlets :ports="outlets"></outlets>
-    </div>
   </div>
 </template>
 
 <script>
-  // import { draggable } from '@/mixins/draggable';
+  import { draggable } from '@/mixins/draggable2';
+  import { moduleSize } from '../../dimensions';
+
+  import Analyser from '../Analyser';
+  import Comb from '../Comb';
+  import Compressor from '../Compressor';
+  import Delay from '../Delay';
+  import Drive from '../Drive';
+  import Env from '../Env';
+  import LFO from '../LFO';
+  import Mixer from '../Mixer';
+  import NoteIn from '../NoteIn';
+  import OSC from '../OSC';
+  import Reverb from '../Reverb';
+  import VCF from '../Filter';
+  import VCO from '../VCO';
+  import VCA from '../VCA';
+
+  import Debugger from '../Debugger';
+  import Node from '../Node';
 
   export default {
-    // mixins: [draggable],
+    mixins: [draggable],
+    components: {
+      Analyser,
+      Comb,
+      Compressor,
+      Debugger,
+      Delay,
+      Drive,
+      Env,
+      LFO,
+      Mixer,
+      Node,
+      NoteIn,
+      OSC,
+      Reverb,
+      VCF,
+      VCO,
+      VCA
+    },
+
     props: {
-      name: null
-      // col: 0,
-      // row: 0,
-      // inlets: [],
-      // outlets: [],
-      // width: 2
+      module: Object
     },
 
     created() {
-      console.log('dddd', this.coords);
-      console.log('%c[component] Creating %s', 'color: blue', this.name);
-      console.log(this.name, this.col, this.coords, this.width);
+      this.id = this.module.id;
+      this.width = `_${moduleSize[this.module.type][0]}U`;
+
+      console.log('%c[component] Creating %s', 'color: blue', this.module.type);
     },
 
     destroyed() {
-      console.log('Destroying %s ', this.name);
+      console.log('Destroying %s ', this.module.type);
     }
   };
 </script>
