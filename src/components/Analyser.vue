df7c30857129567b08bba5682d573e22d6b255bf
wes hatch <wes.hatch@gmail.com>
2018-02-05 22:02:21 -0500

    updating visualizer

--- src/components/Analyser.vue
+++ src/components/Analyser.vue
@@ -17,6 +17,7 @@
 
 <script>
   import { mapGetters } from 'vuex';
+
   export default {
     props: {
       id: null
@@ -40,7 +41,7 @@
 
     created() {
       this.analyser = this.inlets[0].audio = this.context.createAnalyser();
-      this.analyser.fftSize = 256; // 1024;
+      this.analyser.fftSize = 512; // 1024;
       this.analyser.maxDecibels = -30;
       this.analyser.minDecibels = -100;
 
@@ -53,6 +54,7 @@
           this.loop();
         } else {
           // set buffer to 0 and update display
+          this.visualizer.clearRect(0, 0, 222, 356);
         }
       });
     },
@@ -60,7 +62,7 @@
     destroyed() {
       // this.analyser.disconnect();// this is done in Connection
 
-      // this._buffer: ... Float32Array destroy ...??
+      this._buffer = null; //  ... Float32Array destroy ...??
     },
 
     mounted() {
@@ -75,21 +77,10 @@
           this.analyser.getFloatFrequencyData(this._buffer);
         } else {
           // waveform
-          // if (this.isFunction(AnalyserNode.prototype.getFloatTimeDomainData)) {
           this.analyser.getFloatTimeDomainData(this._buffer);
-          // } else {
-          //   var uint8 = new Uint8Array(this._buffer.length);
-          //   this.analyser.getByteTimeDomainData(uint8);
-          //   // referenced https://github.com/mohayonao/get-float-time-domain-data
-          //   // POLYFILL
-          //   for (var i = 0; i < uint8.length; i++){
-          //     this._buffer[i] = (uint8[i] - 128) * 0.0078125;
-          //   }
-          // }
         }
       },
 
-      // renderSpectrum
       render() {
         const values = this._buffer;
         const length = values.length;
@@ -100,7 +91,9 @@
         this.visualizer.clearRect(0, 0, canvasWidth, canvasHeight);
 
         for (let i = 0, x = 0; i < length; i++) {
-          const val = values[i] + 140; // why 140? no idea. Came from Mozilla docs
+          let val = values[i] + 140; // why 140? no idea. Came from Mozilla docs
+
+          // val = isFinite(val) ? val : 0;
 
           this.visualizer.fillStyle = 'rgba(0, 222, 0, ' + val / 140 + ')';
           // this.visualizer.fillStyle = 'rgb(0, 222, 0)';
@@ -110,23 +103,6 @@
         }
       },
 
-      // renderWaveform(){
-      //   //draw the waveform
-      //   waveContext.clearRect(0, 0, canvasWidth, canvasHeight);
-      //   var values = waveform.analyse();
-      //   waveContext.beginPath();
-      //   waveContext.lineJoin = "round";
-      //   waveContext.lineWidth = 6;
-      //   waveContext.strokeStyle = waveformGradient;
-      //   waveContext.moveTo(0, (values[0] / 255) * canvasHeight);
-      //   for (var i = 1, len = values.length; i < len; i++){
-      //     var val = values[i] / 255;
-      //     var x = canvasWidth * (i / len);
-      //     var y = val * canvasHeight;
-      //     waveContext.lineTo(x, y);
-      //   }
-      // waveContext.stroke();
-
       loop() {
         if (this.power) {
           if (!this.editing && this.ticking) {
@@ -134,8 +110,8 @@
             this.render();
           }
 
-          // console.log(this._buffer);
-          // this.ticking = false;
+          this.ticking = !this.ticking;
+
           window.requestAnimationFrame(this.loop);  // .bind(this)
         }
       }
