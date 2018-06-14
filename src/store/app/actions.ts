bd17842bf6837ec3d0e4e5d6b484624100831866
apathetic <wes.hatch@gmail.com>
2018-06-14 15:16:15 -0400

    fix for restoring patches from localStorage

--- src/store/app/actions.ts
+++ src/store/app/actions.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { api, generateKey } from '../firebase';
 import { _ID, _MODULES, _CONNECTIONS, _PARAMETERS, _NAME, state as DEFAULT } from '../patch';
-import { PatchState } from '../../types/';
+import { AppState, PatchState } from '../../types/';
 
 
 // -----------------------------------------------
@@ -11,15 +11,9 @@ import { PatchState } from '../../types/';
 /**
  * Load a Patch from the Store; patches that have been previously fetched and
  * cached may be loaded directly, else fallback to localStorage.
- *
- * NOTE: we cannot load modules, connections, and parameters all at once.
- *       Modules must be mounted _first_, so that the AudioNode is available
- *       to the Connections; likewise with the Modules' settings being
- *       available to the Parameters.
- *
  * @param  {[type]} commit [description]
- * @param  {[type]} state  [description]
- * @param  {String} key    The key of the patch to load
+ * @param  {AppState} state [description]
+ * @param  {string} key The key of the patch to load
  * @return {void}
  */
 export const loadPatch = ({ commit, state }, key?: string) => {
@@ -35,9 +29,11 @@ export const loadPatch = ({ commit, state }, key?: string) => {
     const base = { name: localStorage.getItem(_NAME) || DEFAULT.name };
 
     try {
-      fromStorage = [_ID, _MODULES, _CONNECTIONS, _PARAMETERS].reduce((acc: PatchState, k: string) => {
+      fromStorage = [_ID, _MODULES, _CONNECTIONS, _PARAMETERS].reduce((acc, k) => {
         const value = localStorage.getItem(k);
-        return value ? acc[k] = JSON.parse(value) : acc;
+
+        if (value) { acc[k] = JSON.parse(value); }
+        return acc;
       }, base);
     } catch (err) { /*  */ }
 
@@ -46,7 +42,11 @@ export const loadPatch = ({ commit, state }, key?: string) => {
 
   console.log('%c Loading patch: %s ', 'background:#666;color:white;font-weight:bold;', patch.name || '(localStorage)');
 
-  commit('LOAD_PATCH', patch);     // load: id, name, modules, and parameterSets
+  // NOTE: we cannot load modules, connections, and parameters all at once.
+  //       Modules must be mounted first, so that all AudioNodes are available
+  //       to the Connections; likewise the Modules' settings need to be
+  //       available before Parameters are instantiated.
+  commit('LOAD_PATCH', patch);     // fetches: id, name, modules, and parameterSets
   commit('LOAD_CONNECTIONS', []);  // first, explicitly destroy all connections
 
   // ensure nodes (+ inlets/outlets) are in the DOM...
