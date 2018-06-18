7506e5621e301cd009d2ad739be0499db570bb7d
apathetic <wes.hatch@gmail.com>
2018-06-18 11:42:46 -0400

    restoring firebase api key

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyBcxE9ynMPhVDdTWVywMAPgx8Tq242FWvE",
+  authDomain: "malformed-synth.firebaseapp.com",
+  databaseURL: "https://malformed-synth.firebaseio.com",
+  storageBucket: "malformed-synth.appspot.com"
+};
+
+module.exports = config;
