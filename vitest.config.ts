30e0c60bfb1d8b03d9beff337290603d741e4ae2
wes hatch <wes.hatch@gmail.com>
2024-12-11 22:24:51 -0800

    fixing vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  // esbuild: {
-  //   jsx: 'transform',
-  // },
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, './src') }
+    ]
+  },
   test: {
     /**
      * options for test in a browser environment
