e1fd18b9b8da9dc8f73eb854517711f8bf300447
wes hatch <wes.hatch@gmail.com>
2025-01-06 23:04:23 -0500

    converting analyser

--- src/components/modules/Analyser/Analyser.vue
+++ src/components/modules/Analyser/Analyser.vue
@@ -16,8 +16,10 @@
 
 
 <script lang="ts">
-  import { defineComponent, inject, ref, watch, onMounted, onUnmounted } from 'vue';
+  import { defineComponent, ref, watch, onMounted, onUnmounted } from 'vue';
+  import { Analyser } from 'tone';
   import { useAppStore } from '@/stores/app';
+
 
   export default defineComponent({
     name: 'Analyser',
@@ -30,16 +32,9 @@
     },
 
     setup (props, { expose }) {
-      const type = 'FFT';
-      const context = inject('context');
+      const analyser = new Analyser('fft', 256);
+      const canvas = ref<HTMLCanvasElement>();
       const store = useAppStore();
-      const canvas = ref();
-
-      const analyser = context.createAnalyser();
-      analyser.fftSize = 256; // 512;
-      analyser.maxDecibels = -20; // max value to represent; any freq bins with amplitude above this will be 255
-      analyser.minDecibels = -90; // min value to represent; any freq bins with amplitude below this will be 0
-
       const inlets = [
         {
           label: 'input',
@@ -47,13 +42,24 @@
         }
       ];
 
-      //           = new Float32Array(analyser.frequencyBinCount);
-      let buffer = new Uint8Array(analyser.frequencyBinCount);
-      let ticking = true;
-
-      let visualizer;
+      let buffer = new Float32Array(analyser.size);
+      let visualizer: CanvasRenderingContext2D;
       let canvasWidth = 0;
       let canvasHeight = 0;
+      let ticking = true;
+
+
+      onUnmounted(() => {
+        // buffer = null; ///  ... Float32Array destroy ...??
+        // buffer.length = 0
+        analyser.dispose();
+      });
+
+      onMounted(() => {
+        visualizer = canvas.value.getContext('2d');
+        canvasWidth = visualizer!.canvas.width;
+        canvasHeight = visualizer!.canvas.height;
+      });
 
       watch(() => store.power, (isOn) => {
         if (isOn) {
@@ -63,69 +69,31 @@
         }
       });
 
-      onUnmounted(() => {
-        buffer = null; //  ... Float32Array destroy ...??
-      });
-
-      onMounted(() => {
-        visualizer = canvas.value.getContext('2d');
-        canvasWidth = visualizer.canvas.width;
-        canvasHeight = visualizer.canvas.height;
-      });
-
-
-      function analyse() {
-        if (type === 'FFT') {
-          analyser.getByteFrequencyData(buffer);
-        } else {
-          analyser.getFloatTimeDomainData(buffer);
-        }
-      }
 
       function render() {
+        const barWidth = canvasWidth / buffer.length;
+        let x = 0;
+
         visualizer.fillStyle = 'rgb(0 0 0)';
         visualizer.fillRect(0, 0, canvasWidth, canvasHeight);
+        visualizer.clearRect(0, 0, canvasWidth, canvasHeight);
 
-        if (type === 'FFT') {
-          const values = buffer;
-          const barWidth = canvasWidth / values.length;
-          let x = 0;
+        for (const val of buffer) {
+          visualizer.fillStyle = `rgba(0,222,0, ${val / 255})`;  // 0 -> 255 when getByteData
+          visualizer.fillRect(x, canvasHeight - val / 2, barWidth, val / 2);
 
-          visualizer.clearRect(0, 0, canvasWidth, canvasHeight);
-
-          for (const val of values) {
-            visualizer.fillStyle = `rgba(0,222,0, ${val / 255})`;  // 0 -> 255 when getByteData
-            visualizer.fillRect(x, canvasHeight - val / 2, barWidth, val / 2);
-
-            x += barWidth + 1;
-          }
-        } else {
-          //   waveContext.clearRect(0, 0, canvasWidth, canvasHeight);
-          //   var values = waveform.analyse();
-          //   waveContext.beginPath();
-          //   waveContext.lineJoin = "round";
-          //   waveContext.lineWidth = 6;
-          //   waveContext.strokeStyle = waveformGradient;
-          //   waveContext.moveTo(0, (values[0] / 255) * canvasHeight);
-          //   for (var i = 1, len = values.length; i < len; i++){
-          //     var val = values[i] / 255;
-          //     var x = canvasWidth * (i / len);
-          //     var y = val * canvasHeight;
-          //     waveContext.lineTo(x, y);
-          //   }
-          // waveContext.stroke();
+          x += barWidth + 1;
         }
       }
 
       function loop() {
         if (store.power) {
           if (!store.isEditing && ticking) {
-            analyse();
+            buffer = <Float32Array>analyser.getValue();
             render();
           }
 
           ticking = !ticking;
-
           window.requestAnimationFrame(loop);
         }
       }
